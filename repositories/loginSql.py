30e1d93f79ea6ef385196c7d718922751ae2477c
Christopher C <chris5cuartas@gmail.com>
2024-04-17 04:03:28 -0400

    manually updating branch, sessions now added

--- repositories/loginSql.py
+++ repositories/loginSql.py
@@ -3,14 +3,14 @@ from psycopg.rows import dict_row
         
 
 #checks if the username and password match with the database
-def login(username, password) -> list[dict[str, any]]:
+def login(username) -> list[dict[str, any]]:
     with get_pool().connection() as conn:
         with conn.cursor(row_factory=dict_row) as cur:
             cur.execute('''
-                        SELECT username, first_name, last_name
+                        SELECT username, first_name, last_name, password
                         FROM users
-                        WHERE username = %s AND password = %s
-                        ''', (username, password))
+                        WHERE username = %s
+                        ''', (username,))
             rows = cur.fetchall()
             return rows
 
@@ -34,4 +34,3 @@ def SignUp(username, password, firstname, lastname) -> list[dict[str, any]]:
                         INSERT INTO users (username, password, first_name, last_name)
                         VALUES (%s, %s, %s, %s)
                         ''', (username, password, firstname, lastname))
- 
